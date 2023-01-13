0daf532a1fc271023390bfbfb5ba1b371b9fcfdf
ELHussary <malo.m25@yahoo.com>
2023-01-13 10:58:02 +0200

    Customize Widths

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ function App() {
     <>
       <h1>Header</h1>
 
-      <Modal buttonText="Login" className="test">
+      <Modal buttonText="Login" position={Positions.MiddleCenter} className="xs">
         {/* Modal content goes here */}
-        <p>Login</p>
+        <div style={{ paddingBottom: "1rem" }}>
+          <h4 style={{ padding: "0.2rem 0" }}>Delete Your Account</h4>
+          <p style={{ padding: "1rem 0", fontSize: "14px" }}>Are you sure you want to delete your account</p>
+          <button>No</button>
+          <button>Yes</button>
+        </div>
       </Modal>
 
       <Modal buttonText="Register" position={Positions.MiddleCenter} className="md">
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/global.css";
 import "./styles/modal.css";
 import "./styles/positions.css";
+import "./styles/widths.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
